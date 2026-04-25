8452790161c5557fafc04f57fde12a5804b739aa
Nabarun Middya <72463614+nabarun91221@users.noreply.github.com>
2026-04-25 15:53:33 +0530

    pushing

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ import express from "express";
 import SchoolRouter from "./src/routes/school.route.js"
 import errorHandler from "./src/shared/middlewares/responceHandlers/error.handler.js";
 import { connectDB } from "./src/configs/sequelize.config.js";
+import { apiReference } from "@scalar/express-api-reference";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const docsPath = path.join(__dirname, "./src/doc");
+console.log(docsPath)
 
 const App = express();
 const BaseUrl = "/api/v1"
 const PORT = process.env.PORT;
+
+
+App.use("/schoolManagementOpenAPI_spec.yaml", express.static(path.join(docsPath, "schoolManagementOpenAPI_spec.yaml")));
+
+App.use(
+    "/docs",
+    apiReference({
+        spec: {
+            url: "/schoolManagementOpenAPI_spec.yaml",
+        },
+        theme: "purple",
+    })
+);
 
 App.use(express.json());
 App.use(express.urlencoded());
@@ -24,6 +46,7 @@ App.listen(PORT, async (e) =>
     await connectDB();
 
     console.log("server is running on port: ", PORT);
+    console.log(`docs is available at: http://localhost:${PORT}/docs`);
 
 })
 
